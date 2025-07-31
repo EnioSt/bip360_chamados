9414cc599ddcabbdbb04b0da2e9edbe6c7b9b887
Enio dos Santos Junior <enio.junior@grupochq.com.br>
2025-07-31 12:09:25 -0300

    estilização pensando em interfaces IOS

--- web/src/styles/global.js
+++ web/src/styles/global.js
@@ -6,6 +6,24 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
+  *, *::before, *::after {
+  box-sizing: border-box;
+}
+
+  input, select {
+  -webkit-touch-callout: none;
+  -webkit-tap-highlight-color: transparent;
+}
+
+input, select {
+  font-size: 16px;
+}
+
+select, input {
+  appearance: none;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+}
 
   body {
     font-family: sans-serif;
